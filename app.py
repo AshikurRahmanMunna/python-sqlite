cc9a35105e8a778c6f1ec3b860162f221a86bde5
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2023-05-11 01:25:06 +0600

    subprocess

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 """send email python"""
+import subprocess
+
+completed = subprocess.run(
+    ["python", "other1.py"], check=False, capture_output=True, text=True)
+
+print(completed.returncode)
+if completed.returncode == 0:
+    print(completed.stdout)
+else:
+    print(completed.stderr)
