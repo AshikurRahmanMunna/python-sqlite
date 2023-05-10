92d164c6d633adfe2cc8e785f5c5937f69cd27d8
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2023-05-11 01:04:47 +0600

    email

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
 """send email python"""
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib
-
-message = MIMEMultipart()
-message["from"] = "Your Name"
-message["to"] = "yourrecipient@example.com"
-message["subject"] = "Test email."
-message.attach(MIMEText("Body"))
-
-with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login("yourEmail@example.com", "yourpassword")
-    smtp.send_message(message)
-    print('Sent...')
